a9abd142379c1044dd0257504034861215e0996a
moriasano <mivkao@gmail.com>
2017-03-14 13:44:18 -0400

    All functionailty working, still needs some styling

---
+++ js/calender.js
@@ -0,0 +1,81 @@
+var DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+              'August', 'September', 'October', 'November', 'December'];
+var DAYS_IN_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function Calender(month, year) {
+    // reference: http://jszen.blogspot.com/2007/03/how-to-build-simple-calendar-with.html?_sm_au_=iFHrQN0DNVhSSRHh
+    this.month = month;
+    this.year = year;
+
+    this.getHTML = function () {
+        // Get the numeric value of the first day of the month
+        var firstDay = new Date(this.year, this.month, 1);
+        var startDay = firstDay.getDay();
+
+        // Get the number of days in the month
+        var monthLength = DAYS_IN_MONTH[this.month];
+
+        // Account for leap year
+        if(this.month == 1 && ((this.year % 4 == 0 && this.year % 100 != 0) || this.year % 400 == 0)) {
+            monthLength = 29
+        }
+
+        // Generate the HTML
+        var html = '<table class="table" id="calender">';
+        html += '<tr id="calender-header">' +
+                '   <td colspan="2" id="prev-container"><button class="btn" id="prev-btn" onclick="prevBtn()">' +
+                '       <span class="glyphicon glyphicon-menu-left"></span></button></td>' +
+                '   <td colspan="3" id="calender-title">' + MONTHS[this.month] + "&nbsp;" + this.year + '</td>' +
+                '   <td colspan="2" id="next-container"><button class="btn" id="next-btn" onclick="nextBtn()">' +
+                '       <span class="glyphicon glyphicon-menu-right"></span></button></td></tr>';
+
+        // Label for days of the week
+        html += '<tr id="calender-day-label-container">';
+        for(var i = 0; i <= 6; i++) {
+            html += '<td class="calender-day-label">' + DAYS[i] + '</td>';
+        }
+        html += '</tr><tr class="calender-row">';
+
+        // Cells of the calender
+        var day = 1;
+        for(i = 0; i < 9; i++) {
+            for(var j = 0; j <= 6; j++) {
+                if(day <= monthLength && (i > 0 || j >= startDay)) {
+                    var id = this.month.toString()+ "-" + day.toString();
+                    html += '<td class="calender-day" id="' + id + '" onclick="expandDay(this.id)">' + day;
+                    day++;
+                } else {
+                    html += '<td>'
+                }
+                html += '</td>'
+            }
+            if(day > monthLength) {
+                break;
+            }
+            else {
+                html += '</tr><tr class="calender-row">'
+            }
+        }
+        html += '</table>';
+
+        return html;
+    }
+}
+
+function Event(event_id, event_date) {
+    this.event_id = event_id;
+    this.event_date = event_date;
+    this.event_name;
+    this.event_start;
+    this.event_end;
+    
+    this.set_name = function (name) {
+        this.event_name = name;
+    };
+    this.set_time = function (start, end) {
+        this.event_start = start;
+        this.event_end = end;
+    }
+    
+}
